7b9b7ea4a8d33d1a31a2794d56402515cb66d3a3
Mavaskk <matteo.manaresi.am@gmail.com>
2026-01-14 00:23:36 +0100

    Setting up landing page

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full">
+      <section className="flex flex-col justify-center pt-20">
+
+        <h1 className="text-4xl text-balance font-semibold md:text-6xl xl:text-8xl text-center">Group travel <br />  no friend <span className="font-normal">Drama</span></h1>
+        <p className="text-gray-500 text-center text-sm md:text-xl mt-5">Skip the endless group chat. Let AI privately negotiate conflicting budgets and schedules to find a trip everyone loves.</p>
+
+      </section>
+
+
+    </div>
+   
+  );
+}
