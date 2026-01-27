5574437b4f1bcb53f5972787a3bf65eb435cfb8c
Mavaskk <matteo.manaresi.am@gmail.com>
2026-01-27 11:34:06 +0100

    Add MUI and Emotion dependencies, create Chip component
    Added @mui/material, @emotion/react, and @emotion/styled to dependencies for UI development. Introduced a new Chip component and updated CreateTravelProfileCard.jsx to utilize it.

---
+++ src/app/components/Chip/Chip.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Chip() {
+
+
+
+
+    return(
+        <div>
+
+        </div>
+    )
+}
--- src/app/components/CreateTravelProfileCard/CreateTravelProfileCard.jsx
+++ src/app/components/CreateTravelProfileCard/CreateTravelProfileCard.jsx
@@ -4,6 +4,49 @@ import Card from "../Card/Card"
 import Button from "../Button/Button"
 import { useForm } from "react-hook-form"
 import Input from "../Input/Input"
+import Slider from '@mui/material/Slider';
+import { styled } from '@mui/material/styles';
+
+    const PrettoSlider = styled(Slider)({
+    color: '#375D06',
+    height: 8,
+    '& .MuiSlider-track': {
+        border: 'none',
+    },
+    '& .MuiSlider-thumb': {
+        height: 24,
+        width: 24,
+        backgroundColor: '#fff',
+        border: '2px solid currentColor',
+        '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+        boxShadow: 'inherit',
+        },
+        '&::before': {
+        display: 'none',
+        },
+    },
+    '& .MuiSlider-valueLabel': {
+        lineHeight: 1.2,
+        fontSize: 12,
+        background: 'unset',
+        padding: 0,
+        width: 32,
+        height: 32,
+        borderRadius: '50% 50% 50% 0',
+        backgroundColor: '#375D06',
+        transformOrigin: 'bottom left',
+        transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+        '&::before': { display: 'none' },
+        '&.MuiSlider-valueLabelOpen': {
+        transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+        },
+        '& > *': {
+        transform: 'rotate(45deg)',
+        },
+    },
+    });
+
+
 
 export default function CreateTravelProfileCard() {
        
@@ -22,13 +65,24 @@ export default function CreateTravelProfileCard() {
     }
 
 
+
+
     return (
         <Card className=" w-full  md:w-120 lg:w-150 md:py-16">
             <form className="flex flex-col gap-5 "  onSubmit={handleSubmit(onSubmit)}>
                 <Input register={register} type={"text"} required={true} name={"destination"} error={errors.name} label={"Destination"} placeholder={"e.g. Mountain"} />
 
                 <div>
-                    <p>1. Your budget</p>
+                    <p>1. Your personal budget</p>
+                    <div className="relative">
+                            <PrettoSlider min={200} max={1500} defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+
+
+                        <p className="absolute top-8 md:top-6">200</p>
+                        <p className="absolute right-0 top-8 md:top-6">1500</p>
+                    
+                    </div>
+
                     {/* Slider */}
                 </div>
                 <div>
